b0a9a116e27937362fc2e8c6ba1c0e3b458b00c9
antallen <antallen@gmail.com>
2021-03-26 15:40:59 +0800

    new files

---
+++ 20210326/ex5_1.php
@@ -0,0 +1,33 @@
+<?php
+//print user name
+function name(){
+    return "Peter";
+}
+//caculate the score level
+function score($num){
+    $level = intval($num/10);
+    switch ($level){
+        case 10:
+        case 9:
+            return "A";
+            break;
+        case 8:
+            return "B";
+            break;
+        case 7:
+            return "C";
+            break;
+        case 6:
+            return "D";
+            break;
+        default:
+            return "E";
+            break;
+    }
+}
+
+//run program
+$name = name();
+$score = score(76);
+printf("姓名: %s , 成績: %s 級", $name, $score);
+?>
