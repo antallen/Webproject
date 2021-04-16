aa8981486e32d198621fe71063362a90186a9f10
antallen <antallen@gmail.com>
2021-04-16 15:43:26 +0800

    New process

---
+++ 20210416/clothes.php
@@ -0,0 +1,35 @@
+<?php
+include "doWash.php";
+
+abstract class Clothes implements doWash{
+    private $name;
+    private $prize;
+
+    public function __construct($outsidename, $outsidePrize){
+        $this->name = $outsidename;
+        $this->prize = $outsidePrize;
+    }
+
+    public function setName($outsidename){
+        //$this->name = $outsidename;
+    }
+    
+    public function getName(){}
+
+    public function setPrize($outsidePrize){
+        //$this->prize = $outsidePrize;
+    }
+
+    public function getPrize(){
+        //return $this->prize;
+    }
+
+    public function doWash(){
+        //return "用洗衣機清洗！";
+    }
+
+    public function __destruct(){
+    }
+}
+
+?>
---
+++ 20210416/paints.php
@@ -0,0 +1,48 @@
+<?php
+include "clothes.php";
+
+class Paint extends Clothes {
+    private $length;
+    private $waist;
+
+    public function __construct($outsideName,$outsidePrize,$outsideLength,$outsideWaist){
+        parent::__construct($outsideName,$outsidePrize);
+        $this->setName($outsideName);
+        $this->setPrize($outsidePrize);
+        $this->length = $outsideLength;
+        $this->waist = $outsideWaist;
+    }
+
+    public function setName($outsideName){
+        $this->name = $outsideName;
+    }
+
+    public function setPrize($outsidePrize){
+        $this->prize = $outsidePrize;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function getPrize(){
+        return $this->prize;
+    }
+    public function getLength(){
+        return $this->length;
+    }
+
+    public function getWaist(){
+        return $this->waist;
+    }
+
+    public function doWash(){
+        $string = "用手洗！再".parent::doWash();
+        return $string;
+        //return "用手洗！";
+    }
+    public function __destruct(){    
+    }
+}
+
+?>
