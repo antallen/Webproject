e37ed1745581ce5cc521c95a22a4477654341705
antallen <antallen@gmail.com>
2021-04-17 23:35:39 +0800

    Modify databases

---
+++ databases/connections.php
@@ -0,0 +1,10 @@
+<?php
+//測試連線是否正常
+try {
+
+   $db = new PDO('mysql:host=192.168.1.32;dbname=hello','peter','World123');
+   if (isset($db)) printf("Connection Success");
+} catch(PDOException $e) {
+    printf("Could not connect to the database: %s ",$e->getMessage());
+}
+?>
