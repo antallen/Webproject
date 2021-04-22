5f46ebe60495359be8fa979697263b7a0fa55db1
antallen <antallen@gmail.com>
2021-04-22 17:03:38 +0800

    Modify database

--- databases/hello.php
+++ databases/hello.php
@@ -1,9 +1,18 @@
 <?php
 
-$id = $_POST['id'];
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
+require_once "connections.php";
+
+$id = trim($_POST['id']);
+$name = trim($_POST['name']);
+$email = trim($_POST['email']);
+$phone = trim($_POST['phone']);
 
 printf($id.$name.$email.$phone);
+/*
+$sql = 'INSERT INTO hello (id,name,email,phone) VALUES (?,?,?,?)';
+$runSQL = $db->prepare($sql);
+$runSQL = $db->exec(array($id,$name,$email,$phone));
+
+printf ("Insert data success ...");
+*/
 ?>
