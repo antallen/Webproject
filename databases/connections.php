dcf61fac7804d9405395f7ee0e3ea8a9d16ea2c6
antallen <antallen@gmail.com>
2021-04-22 18:15:29 +0800

    modify database

--- databases/connections.php
+++ databases/connections.php
@@ -2,7 +2,7 @@
 //測試連線是否正常
 try {
 
-   $db = new PDO('mysql:host=192.168.1.32;dbname=hello','peter','World123');
+   $db = new PDO('mysql:host=172.17.0.2;dbname=hello','peter','World123');
    if (isset($db)) printf("Connection Success");
 } catch(PDOException $e) {
     printf("Could not connect to the database: %s ",$e->getMessage());
--- databases/hello.php
+++ databases/hello.php
@@ -2,17 +2,22 @@
 
 require_once "connections.php";
 
-$id = trim($_POST['id']);
+$id = intval(trim($_POST['id']));
 $name = trim($_POST['name']);
 $email = trim($_POST['email']);
 $phone = trim($_POST['phone']);
 
 printf($id.$name.$email.$phone);
 
-$sql = 'INSERT INTO connects (id,name,email,phone) VALUES (?,?,?,?)';
-$runSQL = $db->prepare($sql);
-$runSQL = $db->exec(array($id,$name,$email,$phone));
+try {
+  $runSQL = $db->prepare('INSERT INTO connects(id,name,email,phone) VALUES (?,?,?,?)');
+  $runSQL->execute(array($id,$name,$email,$phone));
+  $runSQL->fetchAll();
+  printf ("Insert data success ...");
 
-printf ("Insert data success ...");
+} catch (PDOException $e2) {
+    printf("Could not connect to the database: %s ",$e2->getMessage());
+}
+    
 
 ?>
