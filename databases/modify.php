e7fa075a8d3c3d0c7a9e9fb5f2edb2da073dd5bc
antallen <antallen@gmail.com>
2021-05-06 22:05:22 +0800

    Modify databases

--- databases/modify.php
+++ databases/modify.php
@@ -1,20 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <?php
     require_once "connections.php";
 
-    
+    if (is_null($_POST['name'])){
+        echo "<script>alert('退出!');history.back();</script>";
+    }
+    if (!(is_null($_POST['phone']))){
+        $phone = trim($_POST['phone']);
+        $name = trim($_POST['name']);
+        $runSQL = $db->prepare("UPDATE connects SET phone = ? where name = ?;");
+        $runSQL->execute(array($phone,$name));
+    }
+     
+    if (!(is_null($_POST['email']))){
+        $email = trim($_POST['email']);
+        $name = trim($_POST['name']);
+        $runSQL = $db->prepare("UPDATE connects SET email = ? where name = ?;");
+        $runSQL->execute(array($phone,$name));
+    }
 
-
+    echo "<script>alert('退出!');history.back();</script>";
 
 
 ?>
-</body>
-</html>
