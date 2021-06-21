a7db87995a2089a9d0c09d1a7d35b4fcaf13df12
antallen <antallen@gmail.com>
2021-06-21 18:24:58 +0800

    add DB

--- helloMVC/config/dbconnect.php
+++ helloMVC/config/dbconnect.php
@@ -1,11 +1,11 @@
 <?php
 //DB 連線用模組
-namespace kernel;
+namespace config;
 
 use PDO;
 use PDOexception;
 
-class DB {
+class DBconnect {
 
     private static $pdo = null;
     
@@ -18,7 +18,7 @@ class DB {
             $dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8', DB_HOST, DB_NAME);
             $option = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
 
-            return self::$pdo = new PDO($dsn, DB_USER, DB_PASS, $option);
+            return self::$pdo = new PDO($dsn, DB_USER, DB_PASSWORD, $option);
         } catch (PDOException $e) {
             exit($e->getMessage());
         }
